94e1b77980fde6e20fe18ea63c5b2b65b7e0b17a
= <=>
2019-03-30 17:14:15 +0100

    Implement slack API

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 const express       = require('express');
 const request       = require('request');
-const slack         = require('slack');
+const { RTMClient } = require('@slack/rtm-api');
 const bodyParser    = require('body-parser');
 
 require('dotenv').config();
@@ -16,7 +16,13 @@ require('dotenv').config();
  * Configuration
  */
 
-const port          = process.env.PORT || 8080;
+const port          = process.env.PORT || 3000;
+
+const slackAuthToken    = process.env.SLACK_AUTH_TOKEN;
+const slackBotToken     = process.env.SLACK_BOT_TOKEN;
+const bot               = new RTMClient(slackBotToken);
+
+const conversationID    = 'CGC6TEYKG'
 
 /**
  * Express server
@@ -24,14 +30,33 @@ const port          = process.env.PORT || 8080;
 
 const app = express();
 
-app.set('port', port);
 app.use(bodyParser.json());
 
-app.listen();
+app.listen(port, () =>  console.log(`Server listening on port ${port}`));
 
-/**
- * Logs
- */
+bot.start()
+    .catch(console.error);
 
- console.log(`Server listening on port ${port}`);
- 
+bot.on('message', (event) => {
+    console.log(event);
+    let text = 'Random';
+
+    switch(event.text) {
+        case 'Bonjour': 
+            text = 'Bonjour ça va?';
+            break;
+        case 'Oui et toi?':
+            text = 'Pas mal, j\' ai pas de cerveau mais on fait avec';
+            break;
+        case 'Non ça va pas' : 
+            text = 'Ta vie est cool.'
+            break;
+        case 'Ca va?':
+            text = 'Evite ce genre de question, je vais finir par faire une random'; 
+    }
+    if(text != 'Random') {
+    bot.sendMessage(text, conversationID)
+        .then(console.log)
+        .catch(console.error);
+    }
+});
